e7a7470e83d6adc9152419baba0e87c238286877
ungoosed <ungoosed@gmail.com>
2023-11-11 20:34:28 -0800

    made site blue and put more functionality

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,25 @@
 
 function NavBar() {
-    return (
-      <div className="Nav">
-        <p>
-          Dropdown
-        </p>
-      </div>
-    );
-  }
-  
-  export default NavBar;
+  return (
+    <div className="Nav">
+      <button className="Dropdown">
+        <a href="#1">Home</a>
+      </button>
+      <button className="Dropdown">
+        <a href="#1">Achievements</a>
+      </button>
+      <button className="Dropdown">
+        <a href="#1">Projects</a>
+      </button>
+      <button className="Dropdown">
+        <a href="#1">Contact</a>
+      </button>
+      <button className="Dropdown">
+        <a href="#1">Hi</a>
+      </button>
+
+    </div>
+  );
+}
+
+export default NavBar;
