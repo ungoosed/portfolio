3859dcd1aff006a45d41ea4233a786e1a1f116a5
ungoosed <ungoosed@gmail.com>
2023-11-27 19:43:36 -0800

    tweaked content

--- src/cards/Projects/Frosty.js
+++ src/cards/Projects/Frosty.js
@@ -1,8 +1,13 @@
+const frosty = require('../../assets/reindeer-frosty.jpg');
 
 function Frosty() {
     return (
         <div className="OpenedCardContent">
-            <h1>Reindeer Boofer is a good hiker!</h1>
+            <h2>Reindeer Boofer is a good hiker!</h2>
+            <p>
+                Once upon a time, reindeer scaled the great cliffs of frosty mountain. As he was such a good hiker, everyone now gives him free gooseberries when he goes the the local gooseberry stand 
+            </p>
+            <img width='90%' src={frosty} alt="reindeer on top of frosty mountain"></img>
         </div >
 
     );
