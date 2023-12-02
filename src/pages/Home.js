2204b7ba56644ed1a556662a876703bc6bdb9968
ungoosed <ungoosed@gmail.com>
2023-12-01 22:08:12 -0800

    added a home page!

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import './Page.css';
+
+function Projects() {
+    return (
+            <div className="Page">
+                <h1>Asher's Site</h1>
+                <p>This is a cool epic site yippee hooray I amde a home page</p>
+
+
+            </div >
+
+    );
+}
+export const displayContext = createContext()
+
+export default Projects;
